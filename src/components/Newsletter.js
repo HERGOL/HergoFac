a25d55153b72002873e9de652e5d9747821bba57
Yidir Kahlouche <yidirk@protonmail.com>
2022-11-18 19:58:14 +0100

    some changes in newsletter

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -2,40 +2,21 @@ import { useState, useEffect } from "react";
 import { Col, Row, Alert } from "react-bootstrap";
 
 
-export const Newsletter = ({ status, message, onValidated }) => {
-  const [email, setEmail] = useState('');
-
-  useEffect(() => {
-    if (status === 'success') clearFields();
-  }, [status])
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    email &&
-    email.indexOf("@") > -1 &&
-    onValidated({
-      EMAIL: email
-    })
-  }
-
-  const clearFields = () => {
-    setEmail('');
-  }
+export const Newsletter = () => {
+  
 
   return (
       <Col lg={12}>
         <div className="newsletter-bx wow slideInUp">
           <Row>
             <Col lg={12} md={6} xl={5}>
-              <h3>Abonnez-vous à la newsletter<br></br>Pour recevoir des news sur le site et avoir accès au bêta.</h3>
-              {status === 'sending' && <Alert>Sending...</Alert>}
-              {status === 'error' && <Alert variant="danger">{message}</Alert>}
-              {status === 'success' && <Alert variant="success">{message}</Alert>}
+              <h3>Abonnez-vous à la newsletter<br></br>Pour recevoir des news sur le site et avoir accès aux bêta.</h3>
+
             </Col>
             <Col md={6} xl={7}>
               <form   action="https://formspree.io/f/xkneegpo" method="POST">
                 <div className="new-email-bx">
-                  <input type="email" name="email" placeholder="Email Address" />
+                  <input type="email" name="email" placeholder="Email Address" required/>
                   <button type="submit">Envoyer</button>
                 </div>
               </form>
