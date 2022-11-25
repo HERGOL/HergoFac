e65b21d7f0fc438d6754550c69dc743f89f8084c
rxafik <47942255+RXAFIK@users.noreply.github.com>
2022-11-25 22:12:51 +0100

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -235,7 +235,7 @@ export const Projects = () => {
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2}></img>
+      <img className="background-image-right" src={colorSharp2} alt="background"></img>
     </section>
   )
 }
