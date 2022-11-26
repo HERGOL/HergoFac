b68fcbb96cc0d5f6096db02fa487b91929d30a85
rxafik <47942255+RXAFIK@users.noreply.github.com>
2022-11-26 22:37:24 +0100

    updated appjsx

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import './App.css';
 import {BrowserRouter as Router, Switch, Route} from  'react-router-dom';
 import Home from './Pages/index';
 import DpSt from './Pages/Dps/DpSt';
+import DpSh from './Pages/Dps/DpSh';
+import DpBio from './Pages/Dps/DpBio';
+import DpChimie from './Pages/Dps/DpChimie';
+import DpGe from './Pages/Dps/DpGe';
+import DpSm from './Pages/Dps/DpSm';
+import DpMath from './Pages/Dps/DpMath';
+import DpGeo from './Pages/Dps/DpGeo';
+import DpGc from './Pages/Dps/DpGc';
+import DpArch from './Pages/Dps/DpArch';
+
 import NotFound from './Pages/NotFound';
 
 
@@ -12,6 +22,15 @@ function App() {
       <Switch>
         <Route path='/' component={Home} exact />
         <Route path='/Dps/DpSt' component={DpSt} exact/>
+        <Route path='/Dps/DpStSm' component={DpSm} exact/>
+        <Route path='/Dps/DpBio' component={DpBio} exact/>
+        <Route path='/Dps/DpChimie' component={DpChimie} exact/>
+        <Route path='/Dps/DpGe' component={DpGe} exact/>
+        <Route path='/Dps/DpGc' component={DpGc} exact/>
+        <Route path='/Dps/DpMath' component={DpMath} exact/>
+        <Route path='/Dps/DpSh' component={DpSh} exact/>
+        <Route path='/Dps/DpGeo' component={DpGeo} exact/>
+        <Route path='/Dps/DpArch' component={DpArch} exact/>
         <Route path="*" component={NotFound} />
       </Switch>
     </Router>
