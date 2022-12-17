9238c0be043367c2b39e395fffed70fb4c18c04e
Yidir Kahlouche <yidirk@protonmail.com>
2022-12-17 11:20:10 +0100

    add hergoanalytics

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -7,10 +7,19 @@ import { Skills } from "../components/Skills";
 import { Projects } from "../components/Projects";
 import { Contact } from "../components/Contact";
 import { Footer } from "../components/Footer";
+import {useLocation } from 'react-router-dom';
 
 
 
 function Home() {
+  const location = useLocation()
+
+	useAckee(location.pathname, {
+		server: 'https://hergoanalytics.netlify.app/',
+		domainId: '249988e5-05e2-46ab-869d-7ec66e8859f7',
+	}, {
+		ignoreLocalhost: false,
+	})
   return (
     <div className="App">
       <NavBar />
